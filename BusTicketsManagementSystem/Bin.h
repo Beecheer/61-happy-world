a5879356c14f5fafbd2c839bde1b8930bba5e55f
RickyZhu2005 <48852577+RickyZhu2005@users.noreply.github.com>
2025-08-31 22:33:52 +0800

    Final Version of "BusTicketsManagementSystem"
    东南大学交通学院 2025－2026学年暑期学校 计算机综合课程设计（研讨）项目 完成于2025年8月31日

---
+++ BusTicketsManagementSystem/Bin.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#define BIN //BIN：二进制文件读写
+
+#ifdef BIN //文件名选择
+#define FILENAME "bus.bin"
+#else 
+#define FILENAME "bus.txt"
+#endif
---
+++ BusTicketsManagementSystem/ExtraFunction.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <vector>
+#include "BusInfo.h"
+
+void SortbyNo(vector<Timetable> timetables);
+void ShowTimetable_no(vector<Timetable> timetables);
+void SortbyLast(vector<Timetable> timetables);
+void ShowTimetable_last(vector<Timetable> timetables);
+int LogLoad(vector<Timetable>&);
+void Statistics(const vector<Timetable>&, int);
